74f83c1d029b8912dfe0390593710df40c74924e
endisl <endiluamba@gmail.com>
2022-04-06 23:26:27 -0300

    rotations

--- src/dsa/Main.java
+++ src/dsa/Main.java
@@ -1,12 +1,20 @@
 package dsa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {3, 5, 6, 9, 11, 18, 20, 21, 24, 30};
+
+        //Anagrams
+        /*var result= AnagramsHisto.areAnagrams("abccc", "bacc");
+        System.out.println(result);*/
+
+        //Jump Search
+        /*int[] arr = {3, 5, 6, 9, 11, 18, 20, 21, 24, 30};
         var index = JumpSearch.search(arr, 6);
-        System.out.println(index);
+        System.out.println(index);*/
 
         //Insertion Sort
         /*int[] arr = {8, 2, 4, 1, 3, 1};
---
+++ src/dsa/Rotations.java
@@ -0,0 +1,11 @@
+package dsa;
+
+public class Rotations {
+    //Using pointers
+    public boolean areRotations(String str1, String str2) {
+
+        return false;
+    }
+}
+
+

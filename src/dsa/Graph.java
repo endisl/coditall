23ea83242741ae7eac0fb2a6464942a985ad0958
endisl <endiluamba@gmail.com>
2022-04-14 11:54:24 -0300

    graph

--- src/dsa/Graph.java
+++ src/dsa/Graph.java
@@ -1,14 +1,43 @@
 package dsa;
 
-import java.util.ArrayDeque;
-import java.util.HashSet;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 
 public class Graph {
-
     private class Node {
+        private String label;
+        public Node(String label) {
+            this.label = label;
+        }
+        @Override
+        public String toString() {
+            return label;
+        }
+    }
+    private Map<String, Node> nodes = new HashMap<>();
+    private Map<Node, List<Node>> adjacencyList = new HashMap();
 
+    public void addNode(String label) {
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacencyList.putIfAbsent(node, new ArrayList<>());
+    }
+
+    public void addEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        if (fromNode == null)
+            throw new IllegalArgumentException();
+        var toNode = nodes.get(to);
+        if (toNode == null)
+            throw new IllegalArgumentException();
+        adjacencyList.get(fromNode).add(toNode);
+    }
+
+    public void print() {
+        for (var source : adjacencyList.keySet()) {
+            var targets = adjacencyList.get(source);
+            if (!targets.isEmpty())
+                System.out.println(source + " is connected to " + targets);
+        }
     }
 
     public void bfs(String root) {

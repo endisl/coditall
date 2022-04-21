e378f4f0a309a908dc32c2391b4f49ae5b0e93e5
endisl <endiluamba@gmail.com>
2022-04-20 23:04:46 -0300

    buy and sell

--- src/dsa/BuySell.java
+++ src/dsa/BuySell.java
@@ -8,8 +8,18 @@ public class BuySell {
         int minPrice = stockPrices[0];
         int maxProfit = stockPrices[1] - stockPrices[0];
 
-
+        for (int i = 1; i < stockPrices.length; i++) {
+            int currentPrice = stockPrices[i];
+            int potentialProfit = currentPrice - minPrice;
+            maxProfit = Math.max(maxProfit, potentialProfit);
+            minPrice = Math.min(minPrice, currentPrice);
+        }
+        return maxProfit;
     }
 
-
+    public static void main(String[] args) {
+        int[] stockPrices = {10, 7, 5, 8, 11, 9};
+        int maxProfit = getMaxProfit(stockPrices);
+        System.out.println(maxProfit);
+    }
 }

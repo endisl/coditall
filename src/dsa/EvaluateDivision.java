257edd01c49dcfd38435ae84f89c2be4a232f96c
endisl <endiluamba@gmail.com>
2022-04-22 22:46:01 -0300

    evaluate division

---
+++ src/dsa/EvaluateDivision.java
@@ -0,0 +1,12 @@
+package dsa;
+
+import java.util.List;
+
+public class EvaluateDivision {
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        return new double[] {-1};
+    }
+    //LC399
+}

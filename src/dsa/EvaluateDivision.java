20cd2b2337cf437b5354186eb9767bf65577e480
endisl <endiluamba@gmail.com>
2022-04-23 23:14:47 -0300

    evaluate division

--- src/dsa/EvaluateDivision.java
+++ src/dsa/EvaluateDivision.java
@@ -1,12 +1,61 @@
 package dsa;
 
-import java.util.List;
+import java.util.*;
 
+//LC399
 public class EvaluateDivision {
 
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
-
-        return new double[] {-1};
+    public static double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
+        // Build graph
+        Map<String, Map<String, Double>> graph = buildGraph(equations, values);
+        double[] result = new double[queries.length];
+        for (int i = 0; i < queries.length; i ++) {
+            result[i] = getPathWeight(queries[i][0], queries[i][1], new HashSet<>(), graph);
+        }
+        return result;
     }
-    //LC399
+
+    //dfs
+    private static double getPathWeight(String start, String end, Set<String> visited, Map<String, Map<String, Double>> graph) {
+        //reject
+        if (!graph.containsKey(start))
+            return -1.;
+        //accept
+        if (graph.get(start).containsKey(end))
+            return graph.get(start).get(end);
+
+        visited.add(start);
+        for (var neighbor : graph.get(start).entrySet()) {
+            if (!visited.contains(neighbor.getKey())) {
+                double productWeight = getPathWeight(neighbor.getKey(), end, visited, graph);
+                if (productWeight != -1)
+                    return neighbor.getValue() * productWeight;
+            }
+        }
+        return -1.;
+    }
+
+    private static Map<String, Map<String, Double>> buildGraph(String[][] equations, double[] values) {
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        String x, y;
+
+        for (int i = 0; i < equations.length; i++) {
+            x = equations[i][0];
+            y = equations[i][1];
+            graph.putIfAbsent(x, new HashMap<>());
+            graph.get(x).put(y, values[i]);
+            graph.putIfAbsent(y, new HashMap<>());
+            graph.get(y).put(x, 1 / values[i]);
+        }
+        return graph;
+    }
+
+    public static void main(String[] args) {
+        String[][] equations = {{"a","b"},{"b","c"}};
+        double[] values = {2.0,3.0};
+        String[][] queries = {{"a","c"},{"b","a"},{"a","e"},{"a","a"},{"x","x"}};
+
+        var result = EvaluateDivision.calcEquation(equations, values, queries);
+        System.out.println(Arrays.toString(result));
+    }
 }

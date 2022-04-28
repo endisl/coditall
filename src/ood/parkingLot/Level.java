4c77f505d4d94919cd696b62a9221aeafb12c456
endisl <endiluamba@gmail.com>
2022-04-27 22:27:37 -0300

    parking lot

--- src/ood/parkingLot/Level.java
+++ src/ood/parkingLot/Level.java
@@ -2,12 +2,26 @@ package ood.parkingLot;
 
 public class Level {
     private int floor;
-    private ParkingSpot spot;
+    private ParkingSpot[] spots;
     private int availableSpots = 0;
     private static final int SPOTS_PER_ROW = 10;
 
     public Level(int floor, int numberSpots) {
-        //...
+        this.floor = floor;
+        spots = new ParkingSpot[numberSpots];
+        int largeSpots = numberSpots / 4;
+        int motoSpots = numberSpots / 4;
+        int compactSpots = numberSpots - largeSpots - motoSpots;
+        for (int i = 0; i < numberSpots; i++) {
+            VehicleSize size = VehicleSize.Motorcycle;
+            if (i < largeSpots)
+                size = VehicleSize.Large;
+            else if (i < largeSpots + compactSpots)
+                size = VehicleSize.Compact;
+            int row = i / SPOTS_PER_ROW;
+            spots[i] = new ParkingSpot(this, row, i, size);
+        }
+        availableSpots = numberSpots;
     }
 
     public int getAvailableSpots() {

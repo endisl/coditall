0c1b2679f6e14044f87de66d0cf369e5588f434e
endisl <endiluamba@gmail.com>
2022-05-31 23:54:42 -0300

    parking lot

--- src/ood/parkingLot/ParkingSpot.java
+++ src/ood/parkingLot/ParkingSpot.java
@@ -8,25 +8,56 @@ public class ParkingSpot {
     private Level level;
 
     public ParkingSpot(Level lvl, int r, int n, VehicleSize s) {
-        //...
+        level = lvl;
+        row = r;
+        spotNumber = n;
+        spotSize = s;
     }
 
     public boolean isAvailable() {
         return vehicle == null;
     }
+
     public boolean canFitVehicle(Vehicle vehicle) {
-        //...
+        return isAvailable() && vehicle.canFitInSpot(this);
     }
+
     public boolean park(Vehicle v) {
-        //...
+        if (!canFitVehicle(v))
+            return false;
+
+        vehicle = v;
+        vehicle.parkInSpot(this);
+        return true;
     }
+
     public int getRow() {
         return row;
     }
+
     public int getSpotNumber() {
         return spotNumber;
     }
+
+    public VehicleSize getSize() {
+        return spotSize;
+    }
+
     public void removeVehicle() {
-        //...
+        level.spotFreed();
+        vehicle = null;
+    }
+
+    public void print() {
+        if (vehicle == null) {
+            if (spotSize == VehicleSize.Compact)
+                System.out.print("c");
+            else if (spotSize == VehicleSize.Large)
+                System.out.print("l");
+            else if (spotSize == VehicleSize.Motorcycle)
+                System.out.print("m");
+        }
+        else
+            vehicle.print();
     }
 }

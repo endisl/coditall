ea19dc06a744630b548b1c7dce50448b5aab4029
endisl <endiluamba@gmail.com>
2022-06-02 22:35:01 -0300

    command

---
+++ src/ood/behavior/c/ex2/Main.java
@@ -0,0 +1,7 @@
+package ood.behavior.c.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ood/behavior/c/ex2/VideoEditor.java
@@ -0,0 +1,31 @@
+package ood.behavior.c.ex2;
+
+public class VideoEditor {
+
+    private float contrast = 0.5f;
+    private String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void removeText() {
+        this.text = "";
+    }
+
+    public void setContrast(float contrast) {
+        this.contrast = contrast;
+    }
+
+    public float getContrast() {
+        return contrast;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoEditor{" +
+                "contrast=" + contrast +
+                ", text='" + text + '\'' +
+                '}';
+    }
+}

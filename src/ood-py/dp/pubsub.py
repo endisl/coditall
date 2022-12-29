c1614d2ca5009ab1510eb87e752d311ed68d11b1
endisl <endiluamba@gmail.com>
2022-12-29 20:35:03 -0300

    ood

--- src/ood-py/dp/pubsub.py
+++ src/ood-py/dp/pubsub.py
@@ -2,7 +2,7 @@
 publisher-subscriber pattern
 """
 
-#from __future__ import annotations
+from __future__ import annotations
 
 
 class Provider:
@@ -47,3 +47,44 @@ class Subscriber:
 
     def run(self, msg: str) -> None:
         print(f'{self.name} got {msg}')
+
+
+def main():
+    """
+    >>> message_center = Provider()
+
+    >>> tv5 = Publisher(message_center)
+
+    >>> john = Subscriber('john', message_center)
+    >>> john.subscribe('movie')
+    >>> jane = Subscriber('jane', message_center)
+    >>> jane.subscribe('music')
+    >>> babe = Subscriber('joe', message_center)
+    >>> babe.subscribe('cartoon')
+    >>> royon = Subscriber('royon', message_center)
+    >>> royon.subscribe('movie')
+    >>> royon.unsubscribe('movie')
+
+    >>> tv5.publish('movie')
+    >>> tv5.publish('ads')
+    >>> tv5.publish('music')
+    >>> tv5.publish('cartoon')
+    >>> tv5.publish('movie')
+    >>> tv5.publish('movie')
+    >>> tv5.publish('blank')
+    >>> tv5.publish('music')
+
+    >>> message_center.update()
+    john got movie
+    jane got music
+    joe got cartoon
+    john got movie
+    john got movie
+    jane got music
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+
+    doctest.testmod()

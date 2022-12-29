e48ca8e00b6cdcb850dc1b5442a50817e8233637
endisl <endiluamba@gmail.com>
2022-12-29 00:11:04 -0300

    ood

--- src/ood-py/dp/pubsub.py
+++ src/ood-py/dp/pubsub.py
@@ -2,7 +2,7 @@
 publisher-subscriber pattern
 """
 
-from __future__ import annotations
+#from __future__ import annotations
 
 
 class Provider:
@@ -24,3 +24,26 @@ class Provider:
             for sub in self.subscribers.get(msg, []):
                 sub.run(msg)
         self.msg_queue = []
+
+
+class Publisher:
+    def __init__(self, msg_center: Provider) -> None:
+        self.provider = msg_center
+
+    def publish(self, msg: str) -> None:
+        self.provider.notify(msg)
+
+
+class Subscriber:
+    def __init__(self, name: str, msg_center: Provider) -> None:
+        self.name = name
+        self.provider = msg_center
+
+    def subscribe(self, msg: str) -> None:
+        self.provider.subscribe(msg, self)
+
+    def unsubscribe(self, msg: str) -> None:
+        self.provider.unsubscribe(msg, self)
+
+    def run(self, msg: str) -> None:
+        print(f'{self.name} got {msg}')
